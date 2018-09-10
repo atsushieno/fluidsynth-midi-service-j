160c8866498de57e35d156d18f4928e3f0a8f748
Atsushi Eno <atsushieno@gmail.com>
2018-09-11 01:06:01 +0900

    File extension matches were different from original C# code. Applied refactorings.

--- app/src/main/java/name/atsushieno/fluidsynthmidideviceservicej/SynthAndroidExtensions.kt
+++ app/src/main/java/name/atsushieno/fluidsynthmidideviceservicej/SynthAndroidExtensions.kt
@@ -5,29 +5,29 @@ import android.os.Build
 import android.os.storage.StorageManager
 import java.io.File
 
-public class SynthAndroidExtensions
+class SynthAndroidExtensions
 {
     companion object {
-        public fun getSoundFonts (soundFonts : MutableList<String?>, context : Context, predefinedTempPath : String) {
+        fun getSoundFonts (soundFonts : MutableList<String?>, context : Context, predefinedTempPath : String?) {
             // OBB support
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                var obbMgr = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager;
-                var obbs = context.obbDirs.flatMap { d -> d.listFiles { f -> f.extension.equals(".obb") }.asIterable() }; // FIXME: case
-                for (obbDir in obbs.filter { f -> obbMgr.isObbMounted(f.absolutePath) }.map { f -> obbMgr.getMountedObbPath(f.absolutePath) })
-                    for (sf2 in File(obbDir).listFiles { f -> f.extension.equals(".sf2") }) // FIXME: case
-                        soundFonts.add(sf2.absolutePath);
-            }
+            val obbMgr = context.getSystemService(Context.STORAGE_SERVICE) as StorageManager
+            val obbs = context.obbDirs.flatMap { d -> d.listFiles { f -> f.extension == "obb" }.asIterable() } // FIXME: case
+            for (obbDir in obbs.filter { f -> obbMgr.isObbMounted(f.absolutePath) }.map { f -> obbMgr.getMountedObbPath(f.absolutePath) })
+                for (sf2 in File(obbDir).listFiles { f -> f.extension == "sf2" }) // FIXME: case
+                    soundFonts.add(sf2.absolutePath)
 
             // Assets
             for (asset in context.assets.list(""))
                 if (asset.endsWith(".sf2")) // FIXME: case
-                    soundFonts.add(asset);
+                    soundFonts.add(asset)
             // temporary local files for debugging
-            var tempPath = File(predefinedTempPath);
-            if (tempPath.exists())
-                for (sf2 in tempPath.listFiles { f -> f.extension.equals(".sf2") })
-                    if (!soundFonts.any { f -> File(f).name.equals(sf2.name) })
-                        soundFonts.add(sf2.absolutePath);
+            if (predefinedTempPath != null) {
+                val tempPath = File(predefinedTempPath)
+                if (tempPath.exists())
+                    for (sf2 in tempPath.listFiles { f -> f.extension == "sf2" })
+                        if (!soundFonts.any { f -> f != null && File(f).name == sf2.name })
+                            soundFonts.add(sf2.absolutePath)
+            }
         }
     }
 }
