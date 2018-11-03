e3fb5caf8230090cde0583306f186d16ee05053b
Atsushi Eno <atsushieno@gmail.com>
2018-11-03 22:57:55 +0900

    make it an actual MidiDeviceService.

--- app/src/main/java/name/atsushieno/fluidsynthmidideviceservicej/FluidsynthMidiDeviceService.kt
+++ app/src/main/java/name/atsushieno/fluidsynthmidideviceservicej/FluidsynthMidiDeviceService.kt
@@ -1,6 +1,7 @@
 package name.atsushieno.fluidsynthmidideviceservicej
 
 import android.media.midi.MidiDeviceService
+import android.media.midi.MidiDeviceStatus
 import android.media.midi.MidiReceiver
 
 class FluidsynthMidiDeviceService : MidiDeviceService()
@@ -9,7 +10,16 @@ class FluidsynthMidiDeviceService : MidiDeviceService()
 
     override fun onGetInputPortReceivers(): Array<MidiReceiver> {
         if (fluidsynth_receiver == null || fluidsynth_receiver!!.isDisposed())
-            fluidsynth_receiver = FluidsynthMidiReceiver (this)
+            fluidsynth_receiver = FluidsynthMidiReceiver (this.applicationContext)
         return arrayOf (fluidsynth_receiver as MidiReceiver)
+    }
+
+    override fun onDeviceStatusChanged(status: MidiDeviceStatus?) {
+        if (status == null)
+            throw IllegalArgumentException ("null status")
+        if (status.isInputPortOpen(0))
+            if (fluidsynth_receiver != null)
+                fluidsynth_receiver!!.dispose()
+        fluidsynth_receiver = null
     }
 }
