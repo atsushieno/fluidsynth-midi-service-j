c2cb35b6bfa014f2f62c62d4f1f0c21cad1e7a8a
Atsushi Eno <atsushieno@gmail.com>
2019-04-19 17:27:28 +0900

    MidiDriver slight sanitization

--- fluidsynthjna/src/main/java/fluidsynth/MidiDriver.kt
+++ fluidsynthjna/src/main/java/fluidsynth/MidiDriver.kt
@@ -2,20 +2,41 @@ package fluidsynth
 
 import com.sun.jna.Pointer
 
-class HandleMidiEventFunc : FluidsynthLibrary.handle_midi_event_func_t
+
+class MidiDriver(settings: Settings, handler: MidiEventHandler) : FluidsynthObject(library.new_fluid_midi_driver(settings.getHandle(), HandleMidiEventFunc(handler), Pointer.NULL), true)
 {
-    companion object {
-        val library = FluidsynthLibrary.INSTANCE
+    interface MidiEventHandler
+    {
+        fun process(event: MidiEvent) : Int
     }
 
-    override fun apply(data: Pointer, evt: Pointer) : Int {
-        throw NotImplementedError()
+    // TODO
+    class MidiEvent
+    {
+        constructor(evt: Pointer)
+        {
+        }
     }
-}
+
+    class HandleMidiEventFunc : FluidsynthLibrary.handle_midi_event_func_t
+    {
+        companion object {
+            val library = FluidsynthLibrary.INSTANCE
+        }
+
+        val handler : MidiEventHandler
+
+        constructor(handler: MidiEventHandler)
+        {
+            this.handler = handler
+        }
+
+        override fun apply(data: Pointer, evt: Pointer) : Int {
+            return handler.process(MidiEvent(evt))
+        }
+    }
 
 
-class MidiDriver(settings: Settings, handler: HandleMidiEventFunc) : FluidsynthObject(library.new_fluid_midi_driver(settings.getHandle(), handler, Pointer.NULL), true)
-{
     companion object {
         val library = FluidsynthLibrary.INSTANCE
     }
